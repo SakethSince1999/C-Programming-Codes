49f41d76daecf9bb6920899ce6834d975a51a58e
Saketh Kumar <134185082+SakethSince1999@users.noreply.github.com>
2024-12-28 19:19:28 +0530

    Update Program0001.c

--- Program0001.c
+++ Program0001.c
@@ -3,7 +3,7 @@
 #include<stdio.h>
 //#include is a preprocessor directive that includes the (stdio.h) header file, which declares functions like `scanf` and `printf`.
 
-int main()  //Function with the name "main" and return type is "integer value(int)"
+int main()  //"main" function with return type is "integer value(int)"
 {
     printf("Hello World");  //Prints "Hello World" text
     return 0;   //Indicates successful Execution without any errors.
