61dbdec06e51d73af0187d5fe5ea22e7d8732453
Saketh Kumar <134185082+SakethSince1999@users.noreply.github.com>
2024-12-29 18:27:19 +0530

    Print "Hello World" using "printf"

---
+++ Program0001.c
@@ -0,0 +1,10 @@
+//Program0001 - Print "Hello World" text
+
+#include<stdio.h>
+//#include is a preprocessor directive that includes the (stdio.h) header file, which declares functions like `scanf` and `printf`.
+
+int main()  //Function with the name "main" and return type is "integer value(int)"
+{
+    printf("Hello World");  //Prints "Hello World" text
+    return 0;   //Indicates successful Execution without any errors.
+}
